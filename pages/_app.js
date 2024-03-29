c7a579b763ed625d06716c2b8242fbf5b01854e8
Gokul <gokulchandan24@gmail.com>
2024-03-29 17:12:19 +0530

    home component created

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
