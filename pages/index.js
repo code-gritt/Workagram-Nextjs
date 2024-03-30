8368cc940a1c0e1db2fe1b3f77ce3e5a9b85fd9f
Gokul <gokulchandan24@gmail.com>
2024-03-30 14:53:44 +0530

    added sidebar

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,25 @@ import Image from "next/image";
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import Widgets from "@/components/Widgets";
+import { getSession, useSession } from "next-auth/react";
+import { connectToDatabase } from "../util/mongodb";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ posts, articles }) {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/home");
+    },
+  });
+
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -13,6 +28,61 @@ export default function Home() {
         <link rel="icon" href="/suitcase.ico" />
       </Head>
       <Header />
+
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          <Sidebar />
+          {/* <Feed posts={posts} /> */}
+        </div>
+        <Widgets articles={articles} />
+        {/* <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence> */}
+      </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Check if the user is authenticated on the server...
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+    };
+  }
+
+  // Get posts on SSR
+  const { db } = await connectToDatabase();
+  const posts = await db
+    .collection("posts")
+    .find()
+    .sort({ timestamp: -1 })
+    .toArray();
+
+  // Get Google News API
+  const results = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      session,
+      articles: results.articles,
+      posts: posts.map((post) => ({
+        _id: post._id.toString(),
+        input: post.input,
+        photoUrl: post.photoUrl,
+        username: post.username,
+        email: post.email,
+        userImg: post.userImg,
+        createdAt: post.createdAt,
+      })),
+    },
+  };
 }
